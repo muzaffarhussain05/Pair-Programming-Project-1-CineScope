5787b8b3ae3ff143caad73230057a4fc4b6d0b94
Aishaarain <155973754+Aishaarain@users.noreply.github.com>
2025-07-08 20:15:47 +0500

    Added MovieList dummy data to Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,49 @@
-import './App.css'
-import Navbar from './components/Navbar';
-import MovieContextProvider from './context/MovieContextProvider';
+// import './App.css'
+// import Navbar from './components/Navbar';
+// import MovieContextProvider from './context/MovieContextProvider';
 
-function App() {
+// function App() {
   
 
-  return (
-    <MovieContextProvider>
-    <Navbar/>
+//   return (
+//     <MovieContextProvider>
+//     <Navbar/>
     
-    </MovieContextProvider>
-  )
+//     </MovieContextProvider>
+//   )
+// }
+
+// export default App
+
+// working in my system
+import React from "react";
+import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+import Home from "./Pages/Home";
+import WatchList from "./Pages/Watchlist";
+import Moviedetails from "./Pages/MovieDetails";
+import Favourite from "./pages/Favorites"
+
+
+function App() {
+  return (
+    
+      <BrowserRouter>
+       <div className="pt-20 px-4 bg-[#1d0f0f] ">
+        <Navbar />
+       
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/Watchlist" element={<WatchList />} />
+            <Route path="/MovieDetails" element={<Moviedetails />} />
+              <Route path="/Favorites" element={<Favourite />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+   
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,36 @@
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import MovieContextProvider from "./context/MovieContextProvider";
-import {
-  Route,
-  RouterProvider,
-  createBrowserRouter,
-  createRoutesFromElements,
-} from "react-router-dom";
-import Home from "./pages/Home";
+// import { createRoot } from "react-dom/client";
+// import "./index.css";
+// import MovieContextProvider from "./context/MovieContextProvider";
+// import {
+//   Route,
+//   RouterProvider,
+//   createBrowserRouter,
+//   createRoutesFromElements,
+// } from "react-router-dom";
+// import Home from "./pages/Home";
 
-const router = createBrowserRouter(
-  createRoutesFromElements(<Route path="/" element={<Home />}></Route>)
-);
+// const router = createBrowserRouter(
+//   createRoutesFromElements(<Route path="/" element={<Home />}></Route>)
+// );
 
-createRoot(document.getElementById("root")).render(
-  <MovieContextProvider>
-    <RouterProvider router={router} />
-  </MovieContextProvider>
-);
+// createRoot(document.getElementById("root")).render(
+//   <MovieContextProvider>
+//     <RouterProvider router={router} />
+//   </MovieContextProvider>
+// );
+
+// working in my system
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
+
+
