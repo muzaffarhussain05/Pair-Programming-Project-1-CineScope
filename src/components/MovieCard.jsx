3b989a8efd95e6cfb691ee05e6e7ec0a45efb70f
Aishaarain <155973754+Aishaarain@users.noreply.github.com>
2025-07-08 19:37:27 +0500

    Added MovieList dummy data to Home

---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+
+const MovieCard = ({ movie }) => {
+  // const posterUrl = movie.poster_path
+  //   ? `${TMDB_IMAGE_BASE_URL}${POSTER_SIZE}${movie.poster_path}` // Still uses TMDB_IMAGE_BASE_URL
+  //   : 'https://via.placeholder.com/500x750?text=No+Image';
+
+  return (
+    <div className="bg-gray-800 rounded-lg shadow-lg overflow-hidden
+                    transform hover:scale-105 transition-transform duration-300 ease-in-out
+                    cursor-pointer">
+      <img
+        src={movie.poster_path}
+        alt={movie.title || movie.name}
+        className="w-full h-96 object-cover object-center"
+      />
+      <div className="p-4">
+        <h3 className="text-white text-lg font-semibold truncate">
+          {movie.title || movie.name}
+        </h3>
+        {movie.release_date && (
+            <p className="text-gray-400 text-sm mt-1">
+                Release: {new Date(movie.release_date).getFullYear()}
+            </p>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default MovieCard
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
-
+import MovieList from "../components/MovieList"
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className=''>
+  <MovieList/>
+  </div>
   )
 }
 
